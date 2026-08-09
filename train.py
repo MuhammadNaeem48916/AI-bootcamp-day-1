bf8b62c8a70317433e313df9255dd95d59d9ca99
M.Naeem <naeem48916@gmail.com>
2026-08-09 16:19:56 +0500

    initial setup

---
+++ train.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("data/house_prices.csv")
+X = df[["area"]]
+y = df["price"]
+
+model = LinearRegression().fit(X, y)
+print("Model trained. Score:", model.score(X, y))
